c1bd8c83efb2510ef371f49dcf21a689cb0ca233
ishika749 <ishikashah749@gmail.com>
2025-05-12 10:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function firstNonRepeatedChar(str) {
-    const charCount = {};
-
+   function firstNonRepeatedChar(str) {
+ const charCount = {};
     for (let char of str) {
         charCount[char] = (charCount[char] || 0) + 1;
     }
-
     for (let char of str) {
         if (charCount[char] === 1) {
             return char;
         }
     }
-
     return null;
 }
-}
+console.log(firstNonRepeatedChar("aabbcdd"));
+console.log(firstNonRepeatedChar("aabbcc"));
+//const input = prompt("a");
+//for (let index = 0; index < bound; inde++) {
+	
+//}
+//alert(firstNonRepeatedChar(input)); 
 //const input = prompt("Enter a string");
 //alert(firstNonRepeatedChar(input))
